d2d1bc9938339fa4e2f2fba1461c21a6ba013245
hhoussem <houssemeddine.hamrouni@gmail.com>
2020-09-27 08:26:12 +0100

    Add basic calculatePrice methods + prepare tests

--- src/main/java/MarketPricer.java
+++ src/main/java/MarketPricer.java
@@ -1,6 +1,13 @@
+import java.math.BigDecimal;
+
 public class MarketPricer {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello from Market pricer");
     }
+
+    public static BigDecimal calculatePrice(Product product, int totalQuantity) {
+       return product.getUnitPrice().multiply(new BigDecimal(totalQuantity));
+    }
+
 }
---
+++ src/test/java/MarketPricerTest.java
@@ -0,0 +1,47 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class MarketPricerTest {
+
+    @Test
+    public void calculateNormalPrice() {
+        Product product = new Product();
+        product.setName("Milk");
+        product.setUnitPrice(new BigDecimal(2));
+        product.setDeal(null);
+
+        Assert.assertEquals(new BigDecimal(12),MarketPricer.calculatePrice(product, 6));
+    }
+
+    @Test
+    public void calculateFixedPriceQuantityDeal() {
+        FixedPriceQuantityDeal fpqDeal = new FixedPriceQuantityDeal();
+        fpqDeal.setQuantity(3);
+        fpqDeal.setPrice(new BigDecimal("5.99"));
+
+        Product product = new Product();
+        product.setName("Cake");
+        product.setUnitPrice(new BigDecimal("2.50"));
+        product.setDeal(fpqDeal);
+
+        Assert.assertTrue(new BigDecimal("16.98").compareTo(MarketPricer.calculatePrice(product, 8)) == 0);
+    }
+
+    @Test
+    public void calculateFreeQuantityDeal() {
+        FreeQuantityDeal fqDeal = new FreeQuantityDeal();
+        fqDeal.setConditionalQuantity(3);
+        fqDeal.setFreeQuantity(2);
+
+        Product product = new Product();
+        product.setName("Milk");
+        product.setUnitPrice(new BigDecimal(2));
+        product.setDeal(fqDeal);
+
+        Assert.assertEquals(new BigDecimal(16),MarketPricer.calculatePrice(product, 12));
+    }
+
+
+}
